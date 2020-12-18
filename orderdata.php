ba1aaae6feeab07a3dbd2548f007f4f000dbfc10
DarkHorse <32187178+darkhorse-coder@users.noreply.github.com>
2020-12-18 13:41:45 -0600

    Update orderdata.php

--- orderdata.php
+++ orderdata.php
@@ -3,7 +3,7 @@
 	$obj = json_decode( $json );
 	header('Content-Type: application/json; charset=utf-8');
 	header("Access-Control-Allow-Origin: *");
-	$db_connection = pg_connect("host='localhost' dbname='plugin_deb' user='postgres' password='HappyOrdering2017'");
+	$db_connection = pg_connect("host='localhost' dbname='db_name' user='db_username' password='db_password'");
 
 		//$response['result'] =array();
 	$code = $obj->{'orderid'};
